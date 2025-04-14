e2d1115a2e7e1083810a442794e9ee1afed5cadf
Victor Popp Henriksen <victor.popp.henriksen@gmail.com>
2025-04-14 17:23:32 +0200

    Scripts for creating graph data

---
+++ scripts/create_edge_data.py
@@ -0,0 +1,91 @@
+import os
+import pandas as pd
+
+import utm
+
+import sys
+sys.path.append("../")
+
+from src.data.loading import(
+    get_interim_windmill_data_gdf,
+    get_processed_data_df
+)
+
+
+#Path from where to load the windmill data
+WINDMILL_DATA_PATH = "../data/interim/windmill"
+
+#Path to store the edge_feats data
+EDGE_FEATS_STORAGE_PATH = "../data/processed/"
+
+#Filename of the edge feature data
+EDGE_FEAT_FILENAME = "edge_feats.csv"
+
+
+
+def main():
+    #load processed data and 
+    full_df = get_processed_data_df()#get_interim_windmill_data_gdf(WINDMILL_DATA_PATH)
+    processed_windmill_ids = full_df.columns.get_level_values(1).unique()
+    
+    #Load the interim data and ensure only windmills that are present in the 
+    # final dataset is used and ensure that there is only one row per GSRN (aka windmill)
+    wind_df = get_interim_windmill_data_gdf(WINDMILL_DATA_PATH)
+    wind_df = wind_df[wind_df["GSRN"].isin(processed_windmill_ids)]
+    wind_df = wind_df.drop_duplicates(subset=["GSRN", "UTM_x", "UTM_y"])
+    
+    #define index, cols and data to use for pd.Multiindex
+    #the main index - all unique GSRN ids(header 0)
+    index = list(wind_df["GSRN"].unique())
+
+    #list of tuples for the multiindex (header 1)
+    cols = []
+    for id in index:
+        cols.append((id, "dlat"))
+        cols.append((id, "dlon"))
+
+
+    #list of lists - dif between lat/lon of one GSRN to all other GSRN
+    dist_data = []
+    for i in range(len(index)):
+        
+        dist_data_i = []
+        for j in range(len(index)):
+                    
+            if i == j:
+                dist_data_i.append(0.0) #dlat
+                dist_data_i.append(0.0) #dlon
+                
+            else:
+                utm_x_i = wind_df["UTM_x"].iloc[i]
+                utm_y_i = wind_df["UTM_y"].iloc[i]    
+                lat_i, lon_i = utm.to_latlon(utm_x_i, utm_y_i, 32, "u")
+                
+                utm_x_j = wind_df["UTM_x"].iloc[j]
+                utm_y_j = wind_df["UTM_y"].iloc[j]
+                lat_j, lon_j = utm.to_latlon(utm_x_j, utm_y_j, 32, "u")
+            
+                dlat = lat_i - lat_j
+                dlon = lon_i - lon_j
+                dist_data_i.append(dlat)
+                dist_data_i.append(dlon)
+        
+        dist_data.append(dist_data_i)
+
+
+
+
+    #create the multiindex DataFrame
+    edge_feats_df = pd.DataFrame(data=dist_data, 
+                                index=index,
+                                columns=pd.MultiIndex.from_tuples(cols))
+
+    #Store the data
+    edge_feats_df.to_csv(os.path.join(EDGE_FEATS_STORAGE_PATH, EDGE_FEAT_FILENAME))
+          
+    return
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/create_node_data.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+
+import utm
+
+import sys
+sys.path.append("../")
+
+from src.data.loading import(
+    get_processed_data_df,
+    get_raw_windmill_meta_data_df
+)
+
+
+#Path to store the edge_feats data
+NODE_FEATS_STORAGE_PATH = "../data/processed/"
+
+#Filename of the edge feature data
+NODE_FEAT_FILENAME = "node_info.csv"
+
+
+
+def main():
+    #load data and ensure only one row per GSRN (aka windmill)
+    full_df = get_processed_data_df()
+    processed_windmill_ids = full_df.columns.get_level_values(1).unique()
+    
+    #load the raw windmill meta data
+    raw_windmill_meta_df = get_raw_windmill_meta_data_df()
+    #select only windmills that are in the final processed data
+    subset_raw_windmill_df = raw_windmill_meta_df[raw_windmill_meta_df["GSRN"].isin(processed_windmill_ids)]
+    #ensure only one row per windmill ID
+    subset_raw_windmill_df = subset_raw_windmill_df.drop_duplicates(subset=["GSRN", "UTM_x", "UTM_y"])
+    
+    #Convert UTM to lat/lon
+    utm_x = subset_raw_windmill_df["UTM_x"].values
+    utm_y = subset_raw_windmill_df["UTM_y"].values
+    lat, lon = utm.to_latlon(utm_x, utm_y, 32, "u")
+    subset_raw_windmill_df["lat"] = lat
+    subset_raw_windmill_df["lon"] = lon
+    
+    
+    #only store relevant columns
+    node_feature_df = subset_raw_windmill_df[["GSRN", "lat", "lon"]]
+    
+        
+    #store data
+    node_feature_df.to_csv(os.path.join(NODE_FEATS_STORAGE_PATH, NODE_FEAT_FILENAME), index=False)
+          
+    return
+
+
+
+if __name__ == "__main__":
+    main()
